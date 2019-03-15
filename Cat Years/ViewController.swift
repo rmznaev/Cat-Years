b16b0fec4afab4e93fea3afa51ce181bd8e8fd98
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-16 01:34:27 +0400

    Code cleaned

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -11,29 +11,43 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var ageTextField: UITextField!
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     
     @IBAction func getAge(_ sender: UIButton) {
+        
         if let age = ageTextField.text {
+            
             if let ageAsNumber = Int(age) {
+                
                 if ageAsNumber < 20 {
+                    
                     let ageInCatYears = ageAsNumber * 7
+                    
                     resultLabel.text = "Your cat is \(ageInCatYears) in human years"
+                    
                 } else {
+                    
                     resultLabel.text = "Cats live between 15-20 years"
+                    
                 }
+                
             } else {
+                
                 resultLabel.text = "Please enter age as a number"
+                
             }
+            
         }
+        
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
     }
-
-
+    
 }
 
